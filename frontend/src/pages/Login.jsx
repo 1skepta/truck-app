e33f6d3e7d6897c70ce23b1838d820ca7dc5708f
1skepta <prospaul999@gmail.com>
2025-04-07 07:55:58 +0000

    working

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,46 +20,51 @@ function Login() {
   };
 
   return (
-    <div className="text-black bg-[#F3FFFD] flex flex-col justify-between min-h-screen relative">
-      <div className="p-7 flex flex-col items-center bg-[#F3FFFD] rounded-lg">
-        <h2 className="mt-20 mb-10 font-extrabold text-2xl">LOGIN</h2>
-        <form onSubmit={handleLogin} className="flex flex-col w-5/6">
-          <input
-            type="text"
-            placeholder="Username"
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-4 focus:border-gray-200 focus:ring-0"
-          />
-          <button className="bg-[#00cca6] py-2 text-white font-bold rounded-md">
-            Login
-          </button>
-        </form>
+    <div>
+      <h2 className="mt-20 font-extrabold text-2x text-center">LOGIN</h2>
+      <div className="text-black bg-[#F3FFFD] flex flex-col justify-between min-h-screen relative md:flex-row md:px-20 md:mt-10 ">
+        <div className="p-7 flex flex-col items-center bg-[#F3FFFD] rounded-lg md:p-0 md:w-1/2">
+          <form
+            onSubmit={handleLogin}
+            className="flex flex-col w-5/6 md:w-4/6 md:mt-5"
+          >
+            <input
+              type="text"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-4 focus:border-gray-200 focus:ring-0"
+            />
+            <button className="bg-[#00cca6] py-2 text-white font-bold rounded-md">
+              Login
+            </button>
+          </form>
 
-        <p className="mt-4 text-sm">
-          Don’t have an account?{" "}
-          <a href="/register" className="text-[#00cca6] font-bold">
-            Create one
-          </a>
-        </p>
+          <p className="mt-4 text-sm">
+            Don’t have an account?{" "}
+            <a href="/register" className="text-[#00cca6] font-bold">
+              Create one
+            </a>
+          </p>
+        </div>
+
+        <img
+          src="assets/auth.png"
+          alt="background"
+          className="absolute bottom-0 left-0 w-full md:w-1/2 md:static"
+          style={{
+            height: "45vh",
+            objectFit: "cover",
+          }}
+        />
       </div>
-
-      <img
-        src="assets/auth.png"
-        alt="background"
-        className="absolute bottom-0 left-0 w-full"
-        style={{
-          height: "45vh",
-          objectFit: "cover",
-        }}
-      />
     </div>
   );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -42,92 +42,95 @@ function Register() {
   };
 
   return (
-    <div className="text-black flex flex-col justify-between min-h-screen relative bg-[#F3FFFD]">
-      <div className="p-7 flex flex-col items-center bg-[#F3FFFD] rounded-lg overflow-y-auto max-h-[75vh]">
-        <h2 className="mt-10 mb-6 font-extrabold text-2xl">REGISTER</h2>
-        <form onSubmit={handleSubmit} className="flex flex-col w-4/5">
-          <input
-            type="text"
-            name="first_name"
-            placeholder="First Name"
-            onChange={handleChange}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
-          />
-          <input
-            type="text"
-            name="last_name"
-            placeholder="Last Name"
-            onChange={handleChange}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
-          />
-          <input
-            type="text"
-            name="username"
-            placeholder="Preferred Username"
-            onChange={handleChange}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={handleChange}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={handleChange}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
-          />
-          <input
-            type="text"
-            name="license_number"
-            placeholder="License Number"
-            onChange={handleProfileChange}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
-          />
-          <input
-            type="text"
-            name="truck_type"
-            placeholder="Truck Type"
-            onChange={handleProfileChange}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
-          />
-          <input
-            type="number"
-            name="years_of_experience"
-            placeholder="Years of Experience"
-            onChange={handleProfileChange}
-            required
-            className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
-          />
-          <button className="bg-[#00cca6] py-2 text-white font-bold rounded-sm">
-            Register
-          </button>
-        </form>
-        <p className="mt-4 text-sm">
-          Already have an account?{" "}
-          <a href="/" className="text-[#00cca6] font-bold">
-            Login
-          </a>
-        </p>
-      </div>
+    <div className=" bg-[#F3FFFD]">
+      <h2 className="mt-10 mb-6 font-extrabold text-2xl text-center">
+        REGISTER
+      </h2>
+      <div className="text-black flex flex-col justify-between min-h-screen relative bg-[#F3FFFD] md:flex-row md:pl-20">
+        <div className="p-7 flex flex-col items-center bg-[#F3FFFD] rounded-lg overflow-y-auto max-h-[75vh] md:w-1/2 md:max-h-none md:p-0 ">
+          <form onSubmit={handleSubmit} className="flex flex-col w-4/5">
+            <input
+              type="text"
+              name="first_name"
+              placeholder="First Name"
+              onChange={handleChange}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
+            />
+            <input
+              type="text"
+              name="last_name"
+              placeholder="Last Name"
+              onChange={handleChange}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
+            />
+            <input
+              type="text"
+              name="username"
+              placeholder="Preferred Username"
+              onChange={handleChange}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleChange}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
+            />
+            <input
+              type="text"
+              name="license_number"
+              placeholder="License Number"
+              onChange={handleProfileChange}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
+            />
+            <input
+              type="text"
+              name="truck_type"
+              placeholder="Truck Type"
+              onChange={handleProfileChange}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
+            />
+            <input
+              type="number"
+              name="years_of_experience"
+              placeholder="Years of Experience"
+              onChange={handleProfileChange}
+              required
+              className="bg-gray-100 border-2 border-gray-200 rounded-md p-2 mb-2 focus:border-gray-200 focus:ring-0"
+            />
+            <button className="bg-[#00cca6] py-2 text-white font-bold rounded-sm cursor-pointer">
+              Register
+            </button>
+          </form>
+          <p className="mt-4 text-sm">
+            Already have an account?
+            <a href="/" className="text-[#00cca6] font-bold pl-1">
+              Login
+            </a>
+          </p>
+        </div>
 
-      <img
-        src="assets/auth.png"
-        alt="background"
-        className="absolute bottom-0 left-0 w-full"
-        style={{ height: "25vh", objectFit: "cover" }}
-      />
+        <img
+          src="assets/auth.png"
+          alt="background"
+          className="absolute bottom-0 left-0 w-full md:static md:w-1/2 h-[25vh] md:h-auto object-cover"
+        />
+      </div>
     </div>
   );
 }
